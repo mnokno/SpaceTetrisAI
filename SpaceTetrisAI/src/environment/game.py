bc943700cb9e86307beb450aeac5dba6449e4d41
mnokno <prv.nowacki@gmail.com>
2023-04-06 23:42:29 +0100

    Implemented Game class

--- SpaceTetrisAI/src/environment/game.py
+++ SpaceTetrisAI/src/environment/game.py
@@ -45,9 +45,9 @@ class Game:
     Stores score
     """
 
-    __multiplayer: int
+    __multiplier: int
     """
-    Stores multiplayer
+    Stores multiplier
     """
 
     def __init__(self):
@@ -60,4 +60,92 @@ class Game:
         self.__piece_alt = self.__pieceGenerator.get_piece(len(self.__historicData) + 1)
         self.__lives = 3
         self.__score = 0
-        self.__multiplayer = 1
+        self.__multiplier = 1
+
+    def place_piece(self, x: int, y: int) -> None:
+        """
+        Plays the mian piece at specified x, y. Will cause an exception if the play is illegal.
+
+        :param x: X position, has to be between 0 and 4
+        :param y: Y position, has to be between 0 and 4
+        """
+
+        if not self.__board.can_play_piece(self.__piece_main, x, y):
+            raise Exception("Illegal piece play attempted!")
+
+        self.__historicData.append((
+            self.__board.get_board(),
+            self.__piece_main,
+            self.__piece_alt,
+            self.__lives,
+            self.__score,
+            self.__multiplier
+        ))
+
+        self.__board.play_piece(self.__piece_main, x, y)
+        cleared_blocks, cleared_lines = self.__board.clear_lines()
+
+        self.__piece_main = self.__piece_alt
+        self.__piece_alt = self.__pieceGenerator.get_piece(len(self.__historicData) + 1)
+
+        if cleared_blocks == 0:
+            self.__multiplier = 1
+        else:
+            self.__score += cleared_lines * cleared_blocks * 10 * self.__multiplier
+            self.__multiplier += 1
+
+    def swap_pieces(self) -> None:
+        """
+        Swaps the main piece with alternative piece
+        """
+        self.__piece_main, self.__piece_alt = self.__piece_alt, self.__piece_main
+
+    def backtrack(self) -> None:
+        """
+        Goes back in history one step
+        """
+        board, self.__piece_main, self.__piece_alt, self.__lives, self.__score, self.__multiplier \
+            = self.__historicData.pop()
+        self.__board.set_board(board)
+
+    def get_board(self) -> Board:
+        """
+        Getter for board
+        :return: board
+        """
+        return self.__board
+
+    def get_main_piece(self) -> int:
+        """
+        Getter for main piece
+        :return: main piece
+        """
+        return self.__piece_main
+
+    def get_alt_piece(self) -> int:
+        """
+        Getter for alternative piece
+        :return: alternative piece
+        """
+        return self.__piece_alt
+
+    def get_lives(self) -> int:
+        """
+        Getter for lives
+        :return: lives
+        """
+        return self.__lives
+
+    def get_score(self) -> int:
+        """
+        Getter for score
+        :return: score
+        """
+        return self.__score
+
+    def get_multiplayer(self) -> int:
+        """
+        Getter for multiplier
+        :return: multiplier
+        """
+        return self.__multiplier
