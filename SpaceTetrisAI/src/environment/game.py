1d571f64ccae1559b26ea6de441477e909681087
mnokno <prv.nowacki@gmail.com>
2023-04-06 22:54:20 +0100

    Create game.py

---
+++ SpaceTetrisAI/src/environment/game.py
@@ -0,0 +1,9 @@
+from src.environment.piece import Piece
+from src.environment.board import Board
+
+
+class Game:
+    """
+    Implementation of a game environment that can be used to train agents,
+    provides implementation for scoring and efficient backtracking.
+    """
