96efaceee2752cd471dba532122abe198da8119e
mnokno <prv.nowacki@gmail.com>
2023-04-06 15:14:24 +0100

    Created types.h

--- Environment/main.cpp
+++ Environment/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "src/types.h"
+
+using namespace environment;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
---
+++ Environment/src/types.h
@@ -0,0 +1,16 @@
+//
+// Created by kubaa on 06/04/2023.
+//
+
+#ifndef ENVIRONMENT_TYPES_H
+#define ENVIRONMENT_TYPES_H
+
+namespace environment {
+
+    typedef unsigned long long int ulong;
+    typedef unsigned int uint;
+    typedef signed char byte;
+
+}
+
+#endif //ENVIRONMENT_TYPES_H
