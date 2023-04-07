8cc2a023fba6a30db15fa5b47afd90fef18fdbfd
mnokno <prv.nowacki@gmail.com>
2023-04-07 16:44:07 +0100

    Create agent.py

---
+++ SpaceTetrisAI/src/agents/agent.py
@@ -0,0 +1,18 @@
+import abc
+
+
+class Agent(abc.ABCMeta):
+    """
+    Agent interface, used as a template for other agents.
+    """
+
+    @abc.abstractmethod
+    def score(self, board: [int], left_piece: [int], multiplier: [int]) -> int:
+        """
+        Scores the given board based on the board itself, other piece and multiple
+        :param board: Board state in a flat vector
+        :param left_piece: Piece left in  a flat vector
+        :param multiplier: Multiplier
+        :return: Score for this position
+        """
+        pass
