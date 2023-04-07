a6b909887a9dcc7250fbfaa70365e6a4e2f9f4b5
mnokno <prv.nowacki@gmail.com>
2023-04-07 21:52:18 +0100

    Update architectures.py

--- SpaceTetrisAI/src/agents/ml/architectures.py
+++ SpaceTetrisAI/src/agents/ml/architectures.py
@@ -10,17 +10,14 @@ class SimpleNet(nn.Module):
         self.sequential = nn.Sequential(
             # board5x5=25 piece3x3=9 total=34
             nn.Linear(in_features=34, out_features=1000),
-            nn.BatchNorm1d(1000),
             nn.Tanh(),
             nn.Dropout(0.2),
 
             nn.Linear(in_features=1000, out_features=10000),
-            nn.BatchNorm1d(10000),
             nn.Tanh(),
             nn.Dropout(0.2),
 
             nn.Linear(in_features=10000, out_features=1000),
-            nn.BatchNorm1d(1000),
             nn.ReLU(),
             nn.Dropout(0.2),
 
