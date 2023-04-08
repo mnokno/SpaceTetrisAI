576988d8e62f3d2f6c10962a7b8f096d6645956f
mnokno <prv.nowacki@gmail.com>
2023-04-08 23:06:51 +0100

    Update architectures.py

--- SpaceTetrisAI/src/agents/ml/architectures.py
+++ SpaceTetrisAI/src/agents/ml/architectures.py
@@ -9,20 +9,16 @@ class SimpleNet(nn.Module):
 
         self.sequential = nn.Sequential(
             # board5x5=25 piece3x3=9 total=34
-            nn.Linear(in_features=34, out_features=250),
+            nn.Linear(in_features=34, out_features=50),
             nn.Tanh(),
-            # nn.Dropout(0.2),
 
-            nn.Linear(in_features=250, out_features=1000),
+            nn.Linear(in_features=50, out_features=100),
             nn.Tanh(),
-            # nn.Dropout(0.2),
 
-            nn.Linear(in_features=1000, out_features=100),
-            nn.ReLU(),
-            # nn.Dropout(0.2),
+            nn.Linear(in_features=100, out_features=50),
+            nn.Tanh(),
 
-            nn.Linear(in_features=100, out_features=1),
-            nn.Sigmoid()
+            nn.Linear(in_features=50, out_features=1)
         )
 
     def forward(self, x: Tensor) -> Tensor:
