e55465e78a5c44fe0747fcc20a236e36a94b130b
mnokno <prv.nowacki@gmail.com>
2023-04-10 16:27:19 +0100

    Update __init__.py

--- SpaceTetrisAI/src/agents/__init__.py
+++ SpaceTetrisAI/src/agents/__init__.py
@@ -4,3 +4,4 @@ from src.agents.random_agent import RandomAgent
 from src.agents.noise_null_agent import NoiseNullAgent
 from src.agents.smart_agent import SmartAgent
 from src.agents.dnn_agent import DNNAgent
+from src.agents.smarter_agent import SmarterAgent
